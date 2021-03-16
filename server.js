2240b3cf8f392b4f572df4b326af6df7bb100aaf
pritimaya <pritimay414@gmail.com>
2021-03-16 06:32:38 +0530

    initial

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+const ejs = require('ejs');
+const path = require('path');
+
+const expressLayout = require('express-ejs-layouts');
+
+const PORT = process.env.PORT || 4000; 
+
+app.get("/", (request,response)=>{
+
+    //response.send("Hello froms server");
+   // response.render('Hello from server by pritimaya');
+
+   response.render('home');
+
+});
+
+
+///setting the template engine
+app.use(expressLayout);
+
+app.set('views',path.join(__dirname,'/resources/views'))
+
+app.set('view engine','ejs');
+
+
+
+app.listen(PORT,()=>{
+    console.log("listening to port for pritimaya "+ PORT)
+});
